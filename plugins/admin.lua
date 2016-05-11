3422b399ed8b146942844da449dc9a0af7ea1c7b
zxcwe <r.harandi.rh.v2@gmail.com>
2016-05-11 13:45:03 +0430

    clean kossher

--- plugins/admin.lua
+++ plugins/admin.lua
@@ -171,20 +171,6 @@ local function run(msg,matches)
     if matches[1] == "whois" then
       user_info("user#id"..matches[2],user_info_callback,{msg=msg})
     end
-    if matches[1] == "sync_gbans" then
-    	if not is_sudo(msg) then-- Sudo only
-    		return
-    	end
-    	local url = "http://seedteam.org/Teleseed/Global_bans.json"
-    	local SEED_gbans = http.request(url)
-    	local jdat = json:decode(SEED_gbans)
-    	for k,v in pairs(jdat) do
-  		redis:hset('user:'..v, 'print_name', k)
-  		banall_user(v)
-      		print(k, v.." Globally banned")
-    	end
-    end
-    return
 end
 return {
   patterns = {
@@ -201,9 +187,6 @@ return {
 	"^[!/](delcontact) (%d+)$",
         "^[!/](addcontact) (.*) (.*) (.*)$",
 	"^[!/](whois) (%d+)$",
-	"^/(sync_gbans)$"--sync your global bans with seed
   },
   run = run,
 }
---By @imandaneshi :)
---https://github.com/SEEDTEAM/TeleSeed/blob/master/plugins/admin.lua
